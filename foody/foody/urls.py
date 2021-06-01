9e18740b47c1bf8661e8ff0c6f44b3a5527d8b85
Ibrahim Alsaid <ibra437@gmail.com>
2021-06-01 12:14:41 -0700

    Initial Setup for main page

--- foody/foody/urls.py
+++ foody/foody/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from foodyapp import urls as foodyapp_urls
 
 urlpatterns = [
+    path('', include(foodyapp_urls)),
     path('admin/', admin.site.urls),
 ]
