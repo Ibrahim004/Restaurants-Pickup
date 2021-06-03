53fd492097320d10ba996c512b14d5c69caf2d40
Ibrahim Alsaid <ibra437@gmail.com>
2021-06-03 10:17:39 -0700

    Implements initial template to list all restaurants

--- foody/foodyapp/urls.py
+++ foody/foodyapp/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='main')
+    path('', views.index, name='main'),
+    path('restaurants/', views.get_restaurants, name='restaurants'),
+    path('restaurant_details/<str:restaurant_id>', views.get_restaurant_details, name='restaurant_details'),
 ]
--- foody/foodyapp/views.py
+++ foody/foodyapp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Restaurant
 
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello world. This is the main page for Foody!")
+
+
+def get_restaurants(request):
+    all_restaurants = Restaurant.objects.all()
+    return render(request, 'foodyapp/restaurants.html', {'list_of_restaurants': all_restaurants})
+
+
+def get_restaurant_details(request, restaurant_id):
+    pass
