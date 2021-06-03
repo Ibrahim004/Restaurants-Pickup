92da7ff8d161e7d3dfed2664a3753174870c8788
Ibrahim Alsaid <ibra437@gmail.com>
2021-06-03 11:37:23 -0700

    Implements initial template to display menu details

--- foody/foodyapp/views.py
+++ foody/foodyapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Restaurant
+from .models import Restaurant, Menu
 
 
 def index(request):
@@ -18,4 +18,6 @@ def get_restaurant_details(request, restaurant_id):
 
 
 def get_menu_details(request, menu_id):
-    pass
+    menu = Menu.objects.get(id=menu_id)
+    restaurant = menu.restaurant_set.all()[0]
+    return render(request, 'foodyapp/menu_details.html', {'menu': menu, 'restaurant': restaurant})
