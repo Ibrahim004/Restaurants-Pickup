9724c69c1638e597a81cac3d3e6e40ed6dde3d1b
Ibrahim Alsaid <ibra437@gmail.com>
2021-06-05 12:32:35 -0700

    Implements front end for food menu and order summary

--- foody/foodyapp/urls.py
+++ foody/foodyapp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('restaurants/', views.get_restaurants, name='restaurants'),
     path('restaurant_details/<str:restaurant_id>', views.get_restaurant_details, name='restaurant_details'),
     path('menu_details/<str:menu_id>', views.get_menu_details, name='menu_details'),
-    path('review_order/', views.review_order, name='review_order')
+    path('review_order/<str:restaurant_id>/<str:menu_id>', views.review_order, name='review_order'),
+    path('submit_order/<str:restaurant_id>/', views.submit_order, name='submit_order'),
 ]
--- foody/foodyapp/views.py
+++ foody/foodyapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Restaurant, Menu
 
 
@@ -21,11 +21,34 @@ def get_restaurant_details(request, restaurant_id):
 def get_menu_details(request, menu_id):
     menu = Menu.objects.get(id=menu_id)
     restaurant = menu.restaurant_set.all()[0]
-    return render(request, 'foodyapp/menu_details.html', {'menu': menu, 'restaurant': restaurant})
+    # return render(request, 'foodyapp/menu_details.html', {'menu': menu, 'restaurant': restaurant})
+
+    return render(request, 'foodyapp/menu_details2.html', {'menu': menu, 'restaurant': restaurant})
 
 
 # todo: implement function to handle post request for order review
-def review_order(request):
+def review_order(request, restaurant_id, menu_id):
+    context = {'restaurant_name': '', 'list_of_items': [{'name': '', 'quantity': '', 'price': ''}]}
+    restaurant_name = Restaurant.objects.get(id=restaurant_id).name
+    menu = Menu.objects.get(id=menu_id)
+
+    list_of_items = []
+    print(request.POST.keys())
+
+    for item in menu.fooditem_set.all():
+        quantity = request.POST.get(item.id)
+
+        if quantity > 0:
+            list_item = {'name': item.name, 'quantity': quantity, 'price': (quantity * item.price)}
+            list_of_items.append(list_item)
+
+    context['restaurant_name'] = restaurant_name
+    context['list_of_items'] = list_of_items
+
+    return render(request, 'foodyapp/order_details.html', context)
+
+
+def submit_order(request, restaurant_id):
     pass
 
 # todo: implement function to submit order to restaurant
